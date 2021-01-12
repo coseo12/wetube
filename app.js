da3f22030ec25ddf43361a8f5a808b1017afaf9d
coseo12 <momo_12@naver.com>
2021-01-12 20:44:49 +0900

    modify route

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import { userRouter } from './router';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+import routes from './routes';
 
 const app = express();
-
-const handleHome = (req, res) => {
-  res.send('Hello World');
-};
-
-const habdleProfile = (req, res) => {
-  res.send('Profile');
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -21,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', handleHome);
-app.get('/profile', habdleProfile);
-app.use('/user', userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;

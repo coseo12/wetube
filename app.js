de32c824d1621d9e61438836cd05e2e4a4ea3317
coseo12 <momo_12@naver.com>
2021-01-12 19:13:19 +0900

    add middleware and router

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import { userRouter } from './router';
+
+const app = express();
+
+const handleHome = (req, res) => {
+  res.send('Hello World');
+};
+
+const habdleProfile = (req, res) => {
+  res.send('Profile');
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get('/', handleHome);
+app.get('/profile', habdleProfile);
+app.use('/user', userRouter);
+
+export default app;
--- init.js
+++ init.js
@@ -1,16 +1,9 @@
-import express from 'express';
+import app from './app';
 
-const app = express();
 const PORT = 4000;
 
 const handleListening = () => {
   console.log(`Listening on: http://localhost${PORT}`);
 };
-
-const handleHome = (req, res) => {
-  res.send('Hello World');
-};
-
-app.get('/', handleHome);
 
 app.listen(PORT, handleListening);

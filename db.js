2ef25c187ed33423141f38c376a0b2cabde99d3c
coseo12 <momo_12@naver.com>
2021-01-15 20:58:53 +0900

    add fake video

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 123,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: `https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4`,
+    creator: {
+      id: 321,
+      name: 'Sco',
+      email: 'test@test.com',
+    },
+  },
+  {
+    id: 222,
+    title: 'Video super',
+    description: 'This is super',
+    views: 34,
+    videoFile: `https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4`,
+    creator: {
+      id: 112,
+      name: 'jaf',
+      email: 'saf@test.com',
+    },
+  },
+  {
+    id: 132,
+    title: 'Video max',
+    description: 'This is max',
+    views: 55,
+    videoFile: `https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4`,
+    creator: {
+      id: 445,
+      name: 'max',
+      email: 'max@test.com',
+    },
+  },
+  {
+    id: 346,
+    title: 'Video X',
+    description: 'This is X',
+    views: 32,
+    videoFile: `https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4`,
+    creator: {
+      id: 122,
+      name: 'xxx',
+      email: 'xxx@test.com',
+    },
+  },
+];
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,10 @@
 import routes from '../routes';
 
 export const localsMiddleware = (req, res, next) => {
-  res.setHeader('Content-Security-Policy', 'script-src kit.fontawesome.com');
+  res.setHeader(
+    'Content-Security-Policy',
+    `script-src kit.fontawesome.com archive.org`
+  );
   res.locals.siteName = `WeTube`;
   res.locals.routes = routes;
   next();

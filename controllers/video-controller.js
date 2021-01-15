4736733a27cd2673afdb9ed86f4da4328c8294bb
coseo12 <momo_12@naver.com>
2021-01-15 19:41:47 +0900

    add search, join, login

--- controllers/video-controller.js
+++ controllers/video-controller.js
@@ -1,9 +1,22 @@
-export const home = (req, res) => {
-  res.render('home');
+export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render('search', { pageTitle: 'Search', searchingBy });
 };
-export const search = (req, res) => res.render('search');
-export const videos = (req, res) => res.render('videos');
-export const upload = (req, res) => res.render('upload');
-export const videoDetail = (req, res) => res.render('video-detail');
-export const editVideo = (req, res) => res.render('edit-video');
-export const deleteVideo = (req, res) => res.render('delete-video');
+export const videos = (req, res) =>
+  res.render('videos', { pageTitle: 'Videos' });
+
+export const upload = (req, res) =>
+  res.render('upload', { pageTitle: 'Upload' });
+
+export const videoDetail = (req, res) =>
+  res.render('video-detail', { pageTitle: 'Video Detail' });
+
+export const editVideo = (req, res) =>
+  res.render('edit-video', { pageTitle: 'Edit Video' });
+
+export const deleteVideo = (req, res) =>
+  res.render('delete-video', { pageTitle: 'Delete Video' });

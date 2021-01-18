a7a74e1e905102525461bf45a9c5f92181e3fb38
coseo12 <momo_12@naver.com>
2021-01-19 01:09:45 +0900

    add

--- middlewares/index.js
+++ middlewares/index.js
@@ -12,7 +12,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.routes = routes;
   res.locals.user = {
     isAuthenticated: true,
-    id: 321,
+    id: 32,
   };
   next();
 };

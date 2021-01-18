69d65d1f278417c0ee9b2dc0deda55766c935fc2
coseo12 <momo_12@naver.com>
2021-01-18 18:06:22 +0900

    setup webpack

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,7 +6,7 @@ const multerVideo = multer({ dest: 'uploads/videos/' });
 export const localsMiddleware = (req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    `script-src kit.fontawesome.com archive.org`
+    `script-src 'self' 'unsafe-eval' kit.fontawesome.com archive.org`
   );
   res.locals.siteName = `WeTube`;
   res.locals.routes = routes;
